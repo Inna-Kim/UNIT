7e29e54d270b9ac2b9de1b3e12535782ba4eef25
Inna <inna170875@yandex.ru>
2020-06-06 21:28:25 +0300

    First commit

---
+++ docs/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        nav: true,
+        dots: false,
+        // navText: ["Назад", "Вперед"] 
+        loop: true,
+        navSpeed:800
+    });
+});
